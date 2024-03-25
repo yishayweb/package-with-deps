d574e6167c7cbc820f93d6e08169332c8238dca9
yishayweb <yishayweb@gmail.com>
2024-03-26 00:18:00 +0200

    updated text

--- src/methods/isBeforeMoment.ts
+++ src/methods/isBeforeMoment.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export const isBeforeMoment = (a: Date, b: Date) => {
-  const str = 'hello there it in the';
+  const str = 'hello there it in the on';
   return {
     isBefore: moment(a).isBefore(b),
     str: str,
